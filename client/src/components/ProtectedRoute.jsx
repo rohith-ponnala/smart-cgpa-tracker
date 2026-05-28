807f66a58078ef6d53319bf61b5d91255b8c5d4b
rohith-ponnala <rohithponnala149@gmail.com>
2026-05-28 17:13:10 +0530

    Fixed protected route auth issue

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,15 +3,11 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
 
   const user = JSON.parse(
-    sessionStorage.getItem("user")
+    localStorage.getItem("user")
   );
 
-  const token =
-    sessionStorage.getItem("token");
-
-  if (!user || !token) {
-
-    return <Navigate to="/login" />;
+  if (!user?.token) {
+    return <Navigate to="/login" replace />;
   }
 
   return children;
